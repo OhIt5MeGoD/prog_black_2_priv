df2b24e446fbb910eeeb59efc2e77263ff7cf168
Neshesh Rai <pspf06@durham.ac.uk>
2026-04-24 01:13:15 +0100

    Generated hashmap for evaluating cards
    also added some explicit typing

--- blackjack/src/main.rs
+++ blackjack/src/main.rs
@@ -1,12 +1,13 @@
 // Module import
 use rand::prelude::*;
 use prompted::input;
+use std::{collections::{HashMap, hash_map}, vec};
 
 fn main() {
 
     let mut rng = rand::rng();
 
-    let mut deck = ["A","K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2","A","K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3","2","A","K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2","A","K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2"];
+    let mut deck: [&str; 52] = ["A","K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2","A","K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3","2","A","K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2","A","K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2"];
 
     let mut dealer_hand = Vec::new();
     let mut player_hand = Vec::new();
@@ -26,4 +27,36 @@ fn main() {
         }
         
     }
+}
+
+fn calc_hand(hand: &[&str]) -> u32 {
+    let mut value: i32 = 0;
+    let mut aces: i32 = 0;
+
+    let val_map = HashMap::from([
+    ("A", 11),
+    ("K", 10),
+    ("Q", 10),
+    ("J", 10),
+    ("10", 10),
+    ("2", 2), 
+    ("3", 3), 
+    ("4", 4), 
+    ("5", 5), 
+    ("6", 6),
+    ("7", 7), 
+    ("8", 8), 
+    ("9", 9)]);
+
+    for card in hand {
+        if card == "A" {
+            aces += 1
+
+        }
+
+    }
+
+
+
+
 }
